2e6811a2229d61da2cdbb8a1b5c1b627a1fb5302
Haytham El-Gebaly <hsmgebaly@gmail.com>
2023-06-08 17:58:51 -0700

    task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    #Print the num of the arguments list of
+    # print the num of the arguments list
     import sys
 
     count = len(sys.argv) - 1
