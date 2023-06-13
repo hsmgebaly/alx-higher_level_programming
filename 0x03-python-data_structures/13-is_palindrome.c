12bf6c2a7967d5d056bb613f89e4e0359380a852
Haytham El-Gebaly <hsmgebaly@gmail.com>
2023-06-12 19:25:04 -0700

    mand

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,70 @@
+#include "lists.h"
+
+
+listint_t *reverse_listint(listint_t **head);
+int is_palindrome(listint_t **head);
+
+/**
+ * reverse_listint - For reversing a singly-linked listint_t list
+ * @head: Refere to the start of node in the list to reverse it
+ * Return: Refere tothe head of reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *node = *head, *next, *prev = NULL;
+
+	while (node)
+	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
+
+/**
+ * is_palindrome - To check if the singly linked list is a palindrome or not
+ * @head: Refere to the start of the linked list
+ * Return: Return 1 If the linked list is  a palindrome
+ *         and return 0 If the linked list isn't a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *tmp, *rev, *mid;
+	size_t size = 0, i;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+
+	tmp = *head;
+	while (tmp)
+	{
+		size++;
+		tmp = tmp->next;
+	}
+
+	tmp = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+		tmp = tmp->next;
+
+	if ((size % 2) == 0 && tmp->n != tmp->next->n)
+		return (0);
+
+	tmp = tmp->next->next;
+	rev = reverse_listint(&tmp);
+	mid = rev;
+
+	tmp = *head;
+	while (rev)
+	{
+		if (tmp->n != rev->n)
+			return (0);
+		tmp = tmp->next;
+		rev = rev->next;
+	}
+	reverse_listint(&mid);
+
+	return (1);}
