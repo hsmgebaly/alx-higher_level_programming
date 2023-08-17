3e6822f09a817033de7484fbb2fb5e60bd8c5cc3
Haytham El-Gebaly <hsmgebaly@gmail.com>
2023-08-16 19:23:56 -0700

    100

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,3 @@
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server: download (same as 100-not_my_genres.sql)
+-- Write a script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
+
---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,2 @@
+-- Import the database hbtn_0d_tvshows_rate dump to your MySQL server: download
+
