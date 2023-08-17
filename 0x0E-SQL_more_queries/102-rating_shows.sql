699130a382ed48d0f80ef1c69bfb574c8f5d3229
Haytham El-Gebaly <hsmgebaly@gmail.com>
2023-08-16 19:29:36 -0700

    102

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,2 +1,7 @@
 -- Import the database hbtn_0d_tvshows_rate dump to your MySQL server: download
-
+-- Write a script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+SELECT tv_shows.title, SUM(tv_show_ratings.rating) AS rating_sum
+FROM tv_shows
+JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.tv_show_id
+GROUP BY tv_shows.id
+ORDER BY rating_sum DESC;
